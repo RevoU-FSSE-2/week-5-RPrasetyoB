d20c1c41cb4752a0ec87bcb06489fb6f241a03e3
RPB <sumitomo.andi@gmail.com>
2023-07-14 10:31:25 +0700

    edit contact section

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 var back = document.getElementById('prev');
 back.onclick = function () {
     var container = document.getElementById('wrapper');
-    container.scrollLeft -= 320;
+    container.scrollLeft -= 290;
 };
 
 var button = document.getElementById('next');
 button.onclick = function () {
     var container = document.getElementById('wrapper');
-    container.scrollLeft += 320;
+    container.scrollLeft += 290;
 };
 
