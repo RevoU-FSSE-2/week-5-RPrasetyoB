d842c04fce46d78d1959f7e476ed800894bef6ff
RPB <sumitomo.andi@gmail.com>
2023-07-14 23:47:02 +0700

    add transition

--- js/index.js
+++ js/index.js
@@ -1,3 +1,33 @@
+const boxes = document.querySelectorAll('.box');
+window.addEventListener('scroll', checkBoxes);
+checkBoxes();
+function checkBoxes(){
+    const triggerBottom = window.innerHeight / 5 * 4;
+    boxes.forEach(box =>{
+        const boxTop = box.getBoundingClientRect().top;
+        if (boxTop < triggerBottom) {
+            box.classList.add('show');
+        }else {
+            box.classList.remove('show');
+        }
+    })
+}
+
+const boxleft = document.querySelectorAll('.boxleft');
+window.addEventListener('scroll', checkBoxesLeft);
+checkBoxesLeft();
+function checkBoxesLeft(){
+    const triggerBottom = window.innerHeight / 5 * 4;
+    boxleft.forEach(boxleft =>{
+        const boxTop = boxleft.getBoundingClientRect().top;
+        if (boxTop < triggerBottom) {
+            boxleft.classList.add('show');
+        }else {
+            boxleft.classList.remove('show');
+        }
+    })
+}
+
 document.getElementById('toggle').addEventListener('click', function(){
     document.getElementById('nav-mobile').classList.toggle('active');
 })
